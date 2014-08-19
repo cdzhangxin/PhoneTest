9cf901664f39e041eeccd8ad00b129c1ad15147f
ZhangXin <412385732@qq.com>
2014-08-19 15:27:29 +0800

    add zepto.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
-window.onload = function(){
-	$("body").toggle(
-		function(e){
-			e.stopPropagation();
-			$("#user-info").animate({left: "0"}, 1000);
-		},
-		function(e){
-			e.stopPropagation();
-			$("#user-info").animate({left: "-100"}, 1000);
+$(function(){
+	$("body").swipeRight(function(){
+		addLeft();
+//		$("#user-info").css("left", 0);
+//		$("#user-info").animate({
+//			left: "0px"
+//		}, 500);
+	});	
+	
+	$("body").swipeLeft(function(){
+		cutLeft();
+	});
+	
+	$("body").swipeDown(function(){
+		console.log("向下滑动");
+	});
+	
+	$("body").swipeUp(function(){
+		console.log("向上滑动");
+	});
+});
+
+function addLeft(){
+	setTimeout(function(){
+		if ($("#user-info").offset().left <0) {
+			$("#user-info").css("left", ++$("#user-info").offset().left);
+			addLeft();
 		}
-	);
-};
+	}, 3);
+}
+
+function cutLeft(){
+	setTimeout(function(){
+		if ($("#user-info").offset().left > -100) {
+			$("#user-info").css("left", --$("#user-info").offset().left);
+			cutLeft();
+		}
+	}, 3);
+}
